eb28eb0c9dcea8b5d0d6ea0a260854c663b28c31
unknown <parulraj13999@gmail.com>
2021-12-16 11:54:58 +0530

    Gift From Stone

---
+++ server.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const Express = require('express');
+const router = require('./lib');
+
+const app = Express();
+const port = process.env.PORT || 3000;
+
+app.use('/api/', router);
+
+app.get('/', function(req, res){
+  res.redirect('/api');
+});
+
+app.listen(port, function () {
+  console.log('Server started on port', port);
+});
