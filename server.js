3442b021bdb1f3afa36ed92cdca182510c92cd1c
Techno Stone <78304026+TechnoStone-dev@users.noreply.github.com>
2021-12-21 19:08:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const router = require('./lib');
 const cors = require("cors");
 const app = Express();
 const port = process.env.PORT || 3000;
-app.use(cors());
+
 app.use('/api/', router);
 
 app.get('/', function(req, res){
